6e47157e73f26d731a09dfbc34b9209e15fed312
matrinh3 <matrinh33@gmail.com>
2025-05-20 12:22:03 +0700

    ok

--- auto-fill-form-ver2/autofill.js
+++ auto-fill-form-ver2/autofill.js
@@ -28,6 +28,10 @@ async function getData() {
     return data;
 }
 
+
+
+
+
 async function randData() {
     const userConfirm = confirm("Bạn có muốn auto điền thông tin không?");
 
@@ -44,32 +48,36 @@ async function randData() {
     }
 }
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
 randData().then((userAcceptedData) => {
     if (!userAcceptedData) return; // Nếu người dùng không muốn auto thì dừng lại
 
-    setInterval(async () => {
-        const data = await getData();
+    document.addEventListener('mousedown', async function (event) {
+        if (event.button === 0) {
+            const data = await getData();
 
-        fill('paypalAccountData_email', data.email);
-        fill('paypalAccountData_phone', data.sdt);
-        fill('paypalAccountData_password', data.password);
-        fill('paypalAccountData_lastName', data.family);
-        fill('paypalAccountData_middleName', data.middle);
-        fill('paypalAccountData_firstName', data.given);
-        fill('paypalAccountData_identificationNum', data.cccd);
-        fill('paypalAccountData_dob', data.birth);
-        fill('paypalAccountData_address1_0', data.address);
-        fill('paypalAccountData_city_0', data.town);
-        fillSelect('dropdownMenuSelect_paypalAccountData_state_0', data.tinh);
-        fill('paypalAccountData_zip_0', data.tinh_zip);
-        check('paypalAccountData_termsAgree');
-        check('paypalAccountData_marketingOptIn');
+            fill('paypalAccountData_email', data.email);
+            fill('paypalAccountData_phone', data.sdt);
+            fill('paypalAccountData_password', data.password);
+            fill('paypalAccountData_lastName', data.family);
+            fill('paypalAccountData_middleName', data.middle);
+            fill('paypalAccountData_firstName', data.given);
+            fill('paypalAccountData_identificationNum', data.cccd);
+            fill('paypalAccountData_dob', data.birth);
+            fill('paypalAccountData_address1_0', data.address);
+            fill('paypalAccountData_city_0', data.town);
+            fillSelect('dropdownMenuSelect_paypalAccountData_state_0', data.tinh);
+            fill('paypalAccountData_zip_0', data.tinh_zip);
+            check('paypalAccountData_termsAgree');
+            check('paypalAccountData_marketingOptIn');
+            await delay(400); // Đợi 1 giây trước khi click
+            const btn = document.getElementById('paypalAccountData_submit');
+            if (btn) btn.click();
 
-        const btn = document.getElementById('paypalAccountData_submit');
-        if (btn) btn.click();
-
-        const btn2 = document.getElementById('paypalAccountData_emailPassword');
-        if (btn2) btn2.click();
-
-    }, 1000);
+            const btn2 = document.getElementById('paypalAccountData_emailPassword');
+            if (btn2) btn2.click();
+        }
+    });
 });
