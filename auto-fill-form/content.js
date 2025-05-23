60983a8ff573be2c358211af58570c48f1879a49
matrinh3 <matrinh33@gmail.com>
2025-05-24 04:28:09 +0700

    ok

--- auto-fill-form/content.js
+++
@@ -1,6 +0,0 @@
-// const script = document.createElement('script');
-// script.src = chrome.runtime.getURL('inject.js');
-// script.onload = function () {
-//     this.remove();
-// };
-// document.documentElement.appendChild(script);
